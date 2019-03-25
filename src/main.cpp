801e1a157273950f90d01b7c603665bc893b18b4
rensandao <38071371+rensandao@users.noreply.github.com>
2019-03-26 00:12:59 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@ string hasData(string s) {
 
 double distance(double x1, double y1, double x2, double y2)
 {
-	return sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+return sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 }
 int ClosestWaypoint(double x, double y, const vector<double> &maps_x, const vector<double> &maps_y)
 {
@@ -375,34 +375,32 @@ int main() {
           double ref_yaw = deg2rad(car_yaw);
 
           //if previous size is almost empty, use the car as starting reference
-          if(prev_size < 2){
-              //use two points that make the path tangent to the car
-              double prev_car_x = car_x - cos(car_yaw);
-              double prev_car_y = car_y - sin(car_yaw);
+          if (prev_size < 2) {
+          	//use two points that make the path tangent to the car
+						double prev_car_x = car_x - cos(car_yaw);
+						double prev_car_y = car_y - sin(car_yaw);
 
-              ptsx.push_back(prev_car_x);
-              ptsx.push_back(car_x);
+						ptsx.push_back(prev_car_x);
+						ptsx.push_back(car_x);
 
-              ptsy.push_back(prev_car_y);
-              ptsy.push_back(car_y);
-          }
-          // use the previous path's end points as starting reference
-          else{
-              //redefine reference state as previous path and point
-              ref_x = previous_path_x[prev_size -1];
-              ref_y = previous_path_y[prev_size -1];
+						ptsy.push_back(prev_car_y);
+						ptsy.push_back(car_y);
+          } else { 
+						// use the previous path's end points as starting reference
+						//redefine reference state as previous path and point
+						ref_x = previous_path_x[prev_size -1];
+						ref_y = previous_path_y[prev_size -1];
 
-              double ref_x_prev = previous_path_x[prev_size -2];
-              double ref_y_prev = previous_path_y[prev_size -2];
-              ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
+						double ref_x_prev = previous_path_x[prev_size -2];
+						double ref_y_prev = previous_path_y[prev_size -2];
+						ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
 
-              //use two points that make the path tangent to the previous path's end point
-              ptsx.push_back(ref_x_prev);
-              ptsx.push_back(ref_x);
+						//use two points that make the path tangent to the previous path's end point
+						ptsx.push_back(ref_x_prev);
+						ptsx.push_back(ref_x);
 
-              ptsy.push_back(ref_y_prev);
-              ptsy.push_back(ref_y);
-
+						ptsy.push_back(ref_y_prev);
+						ptsy.push_back(ref_y);
           }
 
           //In Frenet add evenly 30m spaced points ahead of te starting reference
@@ -418,15 +416,13 @@ int main() {
           ptsy.push_back(next_wp1[1]);
           ptsy.push_back(next_wp2[1]);
 
-          for(int i = 0; i < ptsx.size(); i++){
-
+          for (int i = 0; i < ptsx.size(); i++) {
             //shift car reference angle to 0 degrees
             double shift_x = ptsx[i] - ref_x;
             double shift_y = ptsy[i] - ref_y;
 
             ptsx[i] = (shift_x * cos(0-ref_yaw) - shift_y * sin(0-ref_yaw));
             ptsy[i] = (shift_x * sin(0-ref_yaw) + shift_y * cos(0-ref_yaw));
-
           }
 
           //create a spline
@@ -439,8 +435,7 @@ int main() {
           vector<double> next_x_vals;
           vector<double> next_y_vals;
 
-
-          for (int i=0; i < previous_path_x.size(); i++){
+          for (int i=0; i < previous_path_x.size(); i++) {
             next_x_vals.push_back(previous_path_x[i]);
             next_y_vals.push_back(previous_path_y[i]);
           }
@@ -455,7 +450,6 @@ int main() {
           // fill up the rest of our path planner after filling it with previous points, here we will always
           // output 50 points
           for (int i = 1; i <= 50 - previous_path_x.size(); i++) {
-            
             double N = (target_dist / (0.02 * ref_vel / 2.24));
             double x_point = x_add_on + (target_x) / N;
             double y_point = s(x_point);
@@ -471,11 +465,9 @@ int main() {
             x_point += ref_x;
             y_point += ref_y;
 
-
             next_x_vals.push_back(x_point);
             next_y_vals.push_back(y_point);
           }
-
 
           json msgJson;
 
@@ -486,7 +478,6 @@ int main() {
 
           //this_thread::sleep_for(chrono::milliseconds(1000));
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-          
         }
       } else {
         // Manual driving
@@ -511,13 +502,13 @@ int main() {
   });
 
   h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
-      std::cout << "Connected!!!" << std::endl;
+    std::cout << "Connected!!!" << std::endl;
   });
 
   h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
                           char *message, size_t length) {
-      ws.close();
-      std::cout << "Disconnected" << std::endl;
+		ws.close();
+		std::cout << "Disconnected" << std::endl;
   });
 
     int port = 4567;
@@ -527,5 +518,5 @@ int main() {
       std::cerr << "Failed to listen to port" << std::endl;
       return -1;
     }
-    h.run();
+  h.run();
 }
